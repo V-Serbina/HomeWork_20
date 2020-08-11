25220205cccb1c29b9ba0ef887a8f4ba0e149201
V-Serbina <v.serbina@i.ua>
2020-08-11 21:36:36 +0300

    first commit

---
+++ src/lera/homeWork20/avgSum/Main.java
@@ -0,0 +1,21 @@
+package lera.homeWork20.avgSum;
+
+public class Main {
+    public static void avgSum(float b) {
+        float a = 0;
+        for (int i = 1; i <= b; i++) {
+            a += i;
+            //System.out.println(a);
+        }
+        //System.out.println(a);
+        float s = a / b;
+        System.out.println("avgSum is = " + s);
+
+    }
+
+    public static void main(String[] args) {
+
+        avgSum(100);
+
+    }
+}
